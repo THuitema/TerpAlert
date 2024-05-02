27ce11e27fc805f8d052f3bcd05f35bd50721680
Thomas Huitema <thuitema35@gmail.com>
2024-05-02 01:03:23 -0400

    Created DiningHall class

---
+++ dining_hall_alert/umd.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://nutrition.umd.edu"
+MENU_TAG = "a"
+MENU_CLASS = "menu-item-name"
+
+
+class DiningHall:
+    def __init__(self, name, location_num):
+        self.name = name
+        self.location_num = location_num
+        self.__url = self.__create_url()
+        self.menu = self.__get_menu()
+        # self.__menu_requested = False
+
+    def __create_url(self):
+        month = 5  # make these dynamic later
+        day = 2
+        year = 2024
+        return BASE_URL + "/?locationNum=" + str(self.location_num) + "&dtdate=" + str(month) + "/" + str(day) + "/" + str(year)
+
+    def __get_menu(self):
+        page = requests.get(self.__url)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        items = set()
+        for line in soup.find_all(MENU_TAG, class_=MENU_CLASS):
+            items.add(line.text)
+        # return set(soup.find_all(MENU_TAG, class_=MENU_CLASS))
+        return items
+
+    def print_menu(self):
+        for item in self.menu:
+            print(item)
+
+    def menu_contains(self, item: str):
+        return item in self.menu  # item
+
+
+
+
+
+
