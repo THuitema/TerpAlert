03eb9d56a628ecfb32099cdd8dc1513c535a8bd1
Thomas Huitema <thuitema35@gmail.com>
2024-05-02 10:54:43 -0400

    Added menu_contains() and get_items_by_keyword() methods to umd.py

--- dining_hall_alert/umd.py
+++ dining_hall_alert/umd.py
@@ -18,27 +18,37 @@ class DiningHall:
         month = 5  # make these dynamic later
         day = 2
         year = 2024
-        return BASE_URL + "/?locationNum=" + str(self.location_num) + "&dtdate=" + str(month) + "/" + str(day) + "/" + str(year)
+        return BASE_URL + "/?locationNum=" + str(self.location_num) + "&dtdate=" + str(month) + "/" + str(
+            day) + "/" + str(year)
 
     def __get_menu(self):
         page = requests.get(self.__url)
         soup = BeautifulSoup(page.content, "html.parser")
 
-        items = set()
+        items = list()
         for line in soup.find_all(MENU_TAG, class_=MENU_CLASS):
-            items.add(line.text)
-        # return set(soup.find_all(MENU_TAG, class_=MENU_CLASS))
+            items.append(line.text)
+
         return items
 
     def print_menu(self):
         for item in self.menu:
             print(item)
 
-    def menu_contains(self, item: str):
-        return item in self.menu  # item
+    def menu_contains(self, item: str, partial_check: bool) -> bool:
+        if not partial_check:
+            return item in self.menu
 
+        for entry in self.menu:
+            if item in entry:
+                return True
 
+        return False
 
+    def get_items_by_keyword(self, item: str) -> [str]:
+        items = list()
+        for entry in self.menu:
+            if item in entry:
+                items.append(entry)
 
-
-
+        return items
