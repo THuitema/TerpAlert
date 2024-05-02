b7d7ad6367b2f3ac2056b4de006db8ed3d0df9c5
Thomas Huitema <thuitema35@gmail.com>
2024-05-01 23:26:40 -0400

    Scrape functionality added

--- dining_hall_alert/main.py
+++ dining_hall_alert/main.py
@@ -1,11 +1,13 @@
-from . import soup
+from soup import build_url, process_page
 
 LOCATION_NUMS = {"SOUTH": 16, "YAHENTAMITSI": 19, "251": 51}
 BASE_URL = "https://nutrition.umd.edu"
 
 
 def main():
-    url = soup.build_url(base_url=BASE_URL, location_num=LOCATION_NUMS["SOUTH"])
-    print(url)
+    url = build_url(base_url=BASE_URL, location_num=LOCATION_NUMS["251"])
+    process_page(url)
 
 
+if __name__ == "__main__":
+    main()
--- dining_hall_alert/soup.py
+++ dining_hall_alert/soup.py
@@ -9,3 +9,11 @@ def build_url(base_url, location_num):
     year = 2024
     return base_url + "/?locationNum=" + str(location_num) + "&dtdate=" + str(month) + "/" + str(day) + "/" + str(year)
 
+
+def process_page(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    menu_items = soup.find_all("a", class_="menu-item-name")  # list of "a" tags w/ class "menu-item-name"
+    for item in menu_items:
+        print(item.text)
+
