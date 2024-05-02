76bfa437e810bbea2a7fbc39a85b52cc626a614c
Thomas Huitema <thuitema35@gmail.com>
2024-05-02 15:27:00 -0400

    Generated Django project

--- scrape/main.py
+++ scrape/main.py
@@ -1,5 +1,5 @@
-from soup import build_url, process_page
 from umd import DiningHall
+import django
 
 SOUTH = "South"
 YAHENTAMITSI = "Yahentamitsi"
@@ -17,16 +17,16 @@ def process_commands(dining_halls: list[DiningHall]):
                 print(hall.name + ": " + str(matches))
 
 
-def main():
-
-    the_y = DiningHall(name=YAHENTAMITSI, location_num=HALLS[YAHENTAMITSI])
-    south = DiningHall(name=SOUTH, location_num=HALLS[SOUTH])
-    two_fifty_one = DiningHall(name=TWO_FIFTY_ONE, location_num=HALLS[TWO_FIFTY_ONE])
-
-    dining_halls = [the_y, south, two_fifty_one]
-
-    process_commands(dining_halls)
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     print("Hello World")
+#     # the_y = DiningHall(name=YAHENTAMITSI, location_num=HALLS[YAHENTAMITSI])
+#     # south = DiningHall(name=SOUTH, location_num=HALLS[SOUTH])
+#     # two_fifty_one = DiningHall(name=TWO_FIFTY_ONE, location_num=HALLS[TWO_FIFTY_ONE])
+#     #
+#     # dining_halls = [the_y, south, two_fifty_one]
+#     #
+#     # process_commands(dining_halls)
+#
+#
+# if __name__ == "__main__":
+#     main()
