1d49f532230847d910eb8f574b726f44e1d97b72
Thomas Huitema <thuitema35@gmail.com>
2024-05-02 10:54:18 -0400

    Check items in dining halls via command line

--- dining_hall_alert/main.py
+++ dining_hall_alert/main.py
@@ -1,18 +1,31 @@
 from soup import build_url, process_page
 from umd import DiningHall
 
-LOCATION_NUMS = {"SOUTH": 16, "YAHENTAMITSI": 19, "251": 51}
+SOUTH = "South"
+YAHENTAMITSI = "Yahentamitsi"
+TWO_FIFTY_ONE = "251"
+HALLS = {SOUTH: 16, YAHENTAMITSI: 19, TWO_FIFTY_ONE: 51}
+
+
+def process_commands(dining_halls: list[DiningHall]):
+    while True:
+        request = input("What food would you like to check? ")
+
+        for hall in dining_halls:
+            matches = hall.get_items_by_keyword(item=request)
+            if matches:
+                print(hall.name + ": " + str(matches))
+
 
 def main():
-    # url = build_url(base_url=BASE_URL, location_num=LOCATION_NUMS["251"])
-    # process_page(url)
-    the_y = DiningHall(name="Yahentamitsi", location_num=LOCATION_NUMS["YAHENTAMITSI"])
-    south = DiningHall(name="South", location_num=LOCATION_NUMS["SOUTH"])
-    two_fifty_one = DiningHall(name="251", location_num=LOCATION_NUMS["251"])
-    # the_y.print_menu()
-    print("Does the Y have orange chicken? " + str(the_y.menu_contains(item="Orange Tempura Chicken")))
-    print("Does South have steak? " + str(south.menu_contains(item="Steak")))
-    print("Does 251 have Avocado Toast? " + str(two_fifty_one.menu_contains(item="Avocado Toast")))
+
+    the_y = DiningHall(name=YAHENTAMITSI, location_num=HALLS[YAHENTAMITSI])
+    south = DiningHall(name=SOUTH, location_num=HALLS[SOUTH])
+    two_fifty_one = DiningHall(name=TWO_FIFTY_ONE, location_num=HALLS[TWO_FIFTY_ONE])
+
+    dining_halls = [the_y, south, two_fifty_one]
+
+    process_commands(dining_halls)
 
 
 if __name__ == "__main__":
