dae86dd1317e7971f3537deedd5fe2196078d59a
Thomas Huitema <thuitema35@gmail.com>
2024-05-04 16:48:16 -0400

    Summarize alerts by user

--- scraping/umd.py
+++ scraping/umd.py
@@ -13,11 +13,10 @@ class DiningHall:
         self.location_num = location_num
         self.__url = self.__create_url()
         self.menu = self.__get_menu()
-        # self.__menu_requested = False
 
     def __create_url(self):
         month = 5  # make these dynamic later
-        day = 2
+        day = 4
         year = 2024
         return BASE_URL + "/?locationNum=" + str(self.location_num) + "&dtdate=" + str(month) + "/" + str(
             day) + "/" + str(year)
@@ -32,31 +31,14 @@ class DiningHall:
 
         return items
 
-    def check_for_alerts(self, conn):  # , fields, table, conditions
-        for item in self.menu:
-            item = item.replace("'", "''")  # Replace single quotes w/ double quotes (required for SQL)
-            query = '''
-                SELECT DISTINCT "Users".id, "Users".email, "Keyword".keyword
-                FROM "Users"
-                INNER JOIN "Keyword"
-                ON "Users".id IN (
-                    SELECT user_id
-                    FROM "Keyword"
-                    WHERE keyword = %s
-                )
-                WHERE "Keyword".keyword = %s
-            '''
-            # condition = "keyword = '{0}'".format(item)
-            rows = db_select(conn, query, item, item)  # fields='*', table='Keyword', conditions=condition
-            for row in rows:
-                print("User {0}: alert for {1} at {2} dining hall".format(row[0], row[2], self.name))
-
 
 class Menu:
     def __init__(self, dining_halls: [DiningHall]):
         self.dining_halls = dining_halls
         self.discovered_items = []  # type string
         self.item_list = []  # type Item
+        self.users_to_alert = []  # type User
+        self.discovered_users = []
 
     def create_menu(self):
         for dining_hall in self.dining_halls:  # iterate through dining halls
@@ -73,7 +55,37 @@ class Menu:
                     item_obj = Item(item)
                     item_obj.dining_halls.append(dining_hall.name)
                     self.item_list.append(item_obj)
-        print("Number of unique items: {0}".format(len(self.item_list)))
+
+    def check_for_alerts(self, conn):
+        for item in self.item_list:
+            name = item.name.replace("'", "''")  # replace single quotes w/ double quotes (SQL req.)
+            query = '''
+                SELECT *
+                FROM "Users"
+                WHERE id IN (
+                    SELECT user_id
+                    FROM "Keyword"
+                    WHERE keyword = %s
+                )
+            '''
+            rows = db_select(conn, query, name)
+
+            for row in rows:
+                if row in self.discovered_users:
+                    for obj in self.users_to_alert:
+                        if obj.info == row:
+                            obj.alerts.append(item)
+                else:
+                    self.discovered_users.append(row)
+                    user = User(row)
+                    user.alerts.append(item)
+                    self.users_to_alert.append(user)
+
+    def alert_users(self):
+        out = ''
+        for user in self.users_to_alert:
+            out += str(user) + '\n'
+        print(out)
 
     def __str__(self):
         out = ''
@@ -82,28 +94,24 @@ class Menu:
         return out
 
 
-class Item:
-    def __init__(self, name):
-        self.name = name
-        # self.atYahentamitsi = False
-        # self.atSouth = False
-        # self.at251 = False
-        self.dining_halls = []
+class User:
+    def __init__(self, info):
+        self.info = info
+        self.alerts = []
 
-    # def __build_list(self):
-    #     if self.atYahentamitsi:
-    #         self.dining_halls.append('Yahentamitsi')
-    #     if self.atSouth:
-    #         self.dining_halls.append('South')
-    #     if self.at251:
-    #         self.dining_halls.append('251')
-
-    def __str__(self):
-        # self.__build_list()
-        out = '{0} at '.format(self.name)
-        out += ', '.join(self.dining_halls)
+    def __str__(self):  # use this to alert user by email/sms later?
+        out = 'Alert(s) for {0}\n'.format(self.info)
+        for alert in self.alerts:
+            out += '\t{0}\n'.format(str(alert))
         return out
 
 
+class Item:
+    def __init__(self, name):
+        self.name = name
+        self.dining_halls = []
 
-
+    def __str__(self):
+        out = '{0} at '.format(self.name)
+        out += ', '.join(self.dining_halls)
+        return out
