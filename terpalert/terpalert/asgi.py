1209f763678fb32041e33974d64c090ab2745739
Thomas Huitema <thuitema35@gmail.com>
2024-05-05 19:14:05 -0400

    Restart django

--- terpalert/terpalert/asgi.py
+++ terpalert/terpalert/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dining_hall_alert project.
+ASGI config for terpalert project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dining_hall_alert.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terpalert.settings')
 
 application = get_asgi_application()
--- terpalert/terpalert/wsgi.py
+++ terpalert/terpalert/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dining_hall_alert project.
+WSGI config for terpalert project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dining_hall_alert.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terpalert.settings')
 
 application = get_wsgi_application()
