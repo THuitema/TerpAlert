82a5ce572cd6e19af11e9475a4b4ee09944cec93
Thomas Huitema <thuitema35@gmail.com>
2024-05-07 23:56:10 -0400

    Add login view

--- terpalert/core/urls.py
+++ terpalert/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),  # landing page of website
+    path('login/', views.login_user, name="login"),
 ]
--- terpalert/core/views.py
+++ terpalert/core/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 def home(request):
-    return HttpResponse("Hello User! You are on the home page")
+    # This will serve as our landing page
+    return render(request, 'index.html', {})
 
+
+def login_user(request):
+    if request.method == "POST":
+        # If form was submitted, gather POST request and attempt to log in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Login successful, redirect to account page
+            login(request, user)
+            return redirect('/account/')
+        else:
+            # Login failed
+            messages.success(request, "Invalid Login")
+            return redirect('login/')
+
+    else:
+        # Load webpage if form hasn't been submitted yet
+        return render(request, 'authenticate/login.html', {})
