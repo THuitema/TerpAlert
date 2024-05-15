da08861b2b8f61a46ceff67985390c89c2242351
Thomas Huitema <thuitema35@gmail.com>
2024-05-15 17:42:07 -0400

    Rename model to Profile

--- terpalert/account/admin.py
+++ terpalert/account/admin.py
@@ -3,4 +3,4 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Keyword)
-admin.site.register(User)
+admin.site.register(Profile)
--- terpalert/account/models.py
+++ terpalert/account/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+# Extend auth user model to custom model (or create a one-to-one relationship linking the tables)
 
-class User(models.Model):
-    id = models.IntegerField(primary_key=True)
+
+class Profile(models.Model):
+    # id = models.IntegerField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10)
-    username = models.CharField(max_length=30, default='')
-    email = models.EmailField()
+    # username = models.CharField(max_length=30, default='')
+    # email = models.EmailField()
 
     def __str__(self):
-        return str(self.username)
+        return str(self.user)
 
 
 class Keyword(models.Model):
