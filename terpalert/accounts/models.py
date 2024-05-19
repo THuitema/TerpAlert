d839692f3cedc85789e7299aa97159cf08c909f6
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 18:16:46 -0400

    create_user() in Profile Manager

--- terpalert/accounts/models.py
+++ terpalert/accounts/models.py
@@ -6,7 +6,14 @@ from django.conf import settings
 # Overriding Django's default UserManager with our own, since we are customizing the User model
 class ProfileManager(BaseUserManager):
     def create_user(self, email, phone, password=None):
-        pass
+        if not email:
+            raise ValueError("The given email must be set")
+
+        email = BaseUserManager.normalize_email(email)
+        user = self.model(email=email, phone=phone)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, phone, password=None):
         pass
@@ -18,11 +25,9 @@ class Profile(AbstractBaseUser):
     phone = models.CharField(max_length=40)
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
-
     REQUIRED_FIELDS = ["phone"]  # email already required b/c it is the USERNAME_FIELD
 
-
-
+    objects = ProfileManager()  # links custom user to custom manager, so we can call Profile.objects.create_user()
 
 
 class Keyword(models.Model):
