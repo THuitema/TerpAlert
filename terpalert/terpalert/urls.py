c513d6a452a3fec9060a5c3b2c404e1c52115acc
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 16:35:08 -0400

    Init accounts app and login page

--- terpalert/terpalert/urls.py
+++ terpalert/terpalert/urls.py
@@ -2,7 +2,7 @@
 URL configuration for terpalert project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,15 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('', include('core.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('core.urls')),
 ]
-
-# path('account/', include('django.contrib.auth.urls')),
-
-
-
