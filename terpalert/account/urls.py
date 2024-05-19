7680d66c15fb47aa67605dd2101d360d494f96b7
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 16:33:49 -0400

    Remove old django

--- terpalert/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.account),
-    path('login/', views.login_user, name="login"),
-    path('logout/', views.logout_user, name="logout"),
-    path('signup/', views.signup_user, name="signup"),
-]
