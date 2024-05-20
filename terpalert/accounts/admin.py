7d1fce6dbf72f68d3fa3b0486f5f28caaebbe5a1
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 22:29:16 -0400

    Edit registration for Profile model

--- terpalert/accounts/admin.py
+++ terpalert/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Keyword
+from .models import Profile, Keyword
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+admin.site.register(Profile)  # , UserAdmin
 admin.site.register(Keyword)
