434dbdb8c80d5cb3ae960055c7730161772059ee
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 21:52:26 -0400

    Add documentation

--- terpalert/accounts/tests.py
+++ terpalert/accounts/tests.py
@@ -1,21 +1,29 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your tests here.
 class ProfileManagerTests(TestCase):
-
+    """
+    Tests for the custom user model manager ProfileManager
+    """
     def test_create_user(self):
+        """
+        Testing if create_user() functions as expected
+        """
         Profile = get_user_model()
         user = Profile.objects.create_user(email="test@test.com", phone="1112223333", password="foo")
         self.assertEqual(user.email, "test@test.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-        # try:
-        #     self.assertIsNone(user.username)
-        # except AttributeError:
-        #     pass
+
+        try:
+            # Confirm that the username attribute can no longer be accessed
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
 
         with self.assertRaises(TypeError):
             Profile.objects.create_user()
@@ -25,6 +33,9 @@ class ProfileManagerTests(TestCase):
             Profile.objects.create_user(email="", password="foo")
 
     def test_create_superuser(self):
+        """
+        Testing if create_superuser() functions as expected
+        """
         Profile = get_user_model()
         admin_user = Profile.objects.create_superuser(email="test2@test.com", phone="5555555555", password="bar")
         self.assertEqual(admin_user.email, "test2@test.com")
@@ -33,4 +44,4 @@ class ProfileManagerTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
         with self.assertRaises(ValueError):
-            Profile.objects.create_superuser(email="", password="foo", is_superuser=False)
+            Profile.objects.create_superuser(email="", phone="9999999999", password="foo", is_superuser=False)
