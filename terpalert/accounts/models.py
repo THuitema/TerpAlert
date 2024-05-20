8ca4f8255e658764580ed735e3fbca6327a814b1
Thomas Huitema <thuitema35@gmail.com>
2024-05-19 22:13:07 -0400

    Added has_perm() and has_module_perms() to fix AttributeError when trying to access admin page

--- terpalert/accounts/models.py
+++ terpalert/accounts/models.py
@@ -52,10 +52,15 @@ class Profile(AbstractBaseUser):
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
-    EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ["phone"]  # email already required b/c it is the USERNAME_FIELD
 
     objects = ProfileManager()  # links custom user to custom manager, so we can call Profile.objects.create_user()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
     def __str__(self):
         return self.email
