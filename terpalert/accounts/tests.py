e3ccf504764b6fb0674c0d73315edd8907a8c887
Thomas Huitema <thuitema35@gmail.com>
2024-05-21 16:27:19 -0400

    Create tests for ProfileCreationForm

--- terpalert/accounts/tests.py
+++ terpalert/accounts/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import ProfileCreationForm
+from django import forms
 
 
 # Create your tests here.
@@ -46,3 +48,52 @@ class ProfileManagerTests(TestCase):
 
         with self.assertRaises(ValueError):
             Profile.objects.create_superuser(email="", phone="9999999999", password="foo", is_superuser=False)
+
+
+class ProfileCreationFormTests(TestCase):
+    def test_valid_form(self):
+        form_data = {'email': 'foo@bar.com', 'phone': '1112223333', 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_email(self):
+        form_data = {'email': 'foobar.com', 'phone': '1112223333', 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_phone(self):
+        form_data = {'email': 'foo@bar.com', 'phone': '111222333', 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_password(self):
+        form_data = {'email': 'foo@bar.com', 'phone': '1112223333', 'password1': 'foobarpass1', 'password2': 'foobarpass2'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_clean_email(self):
+        email1 = 'TESTING@test.com'
+        form_data = {'email': email1, 'phone': '1112223333', 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.clean_email() == email1.lower())
+        form.save()
+
+        form_data = {'email': email1, 'phone': '9876543211', 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+
+        form2 = ProfileCreationForm(data=form_data)
+        self.assertFalse(form2.is_valid())  # Email already exists
+
+    def test_clean_phone(self):
+        phone1 = '1112223456'
+        form_data = {'email': 'TESTING@test.com', 'phone': phone1, 'password1': 'foobarpass1', 'password2': 'foobarpass1'}
+        form = ProfileCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.clean_phone() == phone1)
+
+        form_data['phone'] = '111222poiu'
+
+        form2 = ProfileCreationForm(data=form_data)
+        self.assertFalse(form2.is_valid())
+
+
