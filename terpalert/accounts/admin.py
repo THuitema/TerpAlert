60e1925f75754ae4032e41d26b4499babd2c1183
Thomas Huitema <thuitema35@gmail.com>
2024-05-20 20:49:11 -0400

    Remove commented-out code

--- terpalert/accounts/admin.py
+++ terpalert/accounts/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Profile, Keyword
+from .forms import ProfileCreationForm, ProfileChangeForm
 
-# Register your models here.
-admin.site.register(Profile)  # , UserAdmin
+
+# class ProfileAdmin(UserAdmin):
+#     add_form = ProfileCreationForm
+#     model = Profile
+#     list_display = ("email", "phone", "is_staff", "is_active",)
+#     list_filter = ("email", "phone", "is_staff", "is_active",)
+#     filter_horizontal = ("",)
+#     add_fieldsets = (
+#         (None, {
+#             "classes": ("wide",),
+#             "fields": ("email", "phone", "password1", "password2", "is_staff",
+#                        "is_active")  # , "groups", "user_permissions"
+#         }),
+#     )
+#     search_fields = ("email", "phone",)
+#     ordering = ("email", "phone",)
+
+
+admin.site.register(Profile)  #, ProfileAdmin
 admin.site.register(Keyword)
