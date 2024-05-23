7002c76d8c2cb556580cdba98290e92d3808488a
Thomas Huitema <thuitema35@gmail.com>
2024-05-22 22:20:22 -0400

    Don't require STATICFILES_DIRS

--- terpalert/terpalert/settings.py
+++ terpalert/terpalert/settings.py
@@ -132,10 +132,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
