399ee14ca7e3e5b38ffc645eebe0bc9cd8ba80b1
Thomas Huitema <thuitema35@gmail.com>
2024-05-23 21:15:20 -0400

    Add deleteKeyword() and getCookie()

--- terpalert/accounts/static/main.js
+++ terpalert/accounts/static/main.js
@@ -8,24 +8,26 @@ function getKeywords(e) {
         type: 'GET',
         url: '/accounts/load-keywords/',
         success: function (response) {
-            console.log(response.data)
             const data = response.data
+            if(data.length == 0) {
+                e.innerHTML += `
+                    <p>Nothing added yet!</p>
+                `
+            }
             data.forEach(element => {
                 e.innerHTML += `
                     <tr id="${element.id}">
                         
                         <td>${element.keyword}</td>
                         <td>
-                            <button type="button" class="btn btn-danger" onclick="deleteKeyword()">
+                            <button type="button" class="btn btn-danger" onclick="deleteKeyword(this)">
                                 <i class="bi bi-trash"></i>
                             </button>
                         </td>
                         
                     </tr>
                 `
-                // ${element.user}
             });
-
         },
         error: function (error) {
             console.log(error)
@@ -33,8 +35,43 @@ function getKeywords(e) {
     })
 }
 
-function deleteKeyword() {
-    console.log('clicked!')
+function deleteKeyword(button) {
+    const csrftoken = getCookie('csrftoken');
+    const td = button.parentNode;
+    const tr = td.parentNode;
+
+    $.ajax({
+        type: 'POST',
+        url: '/accounts/delete-keyword/',
+        headers: {'X-CSRFToken': csrftoken},
+        data: {
+            'keyword_id': tr.id,
+        },
+        success: function(response) {
+            console.log(response.data)
+            tr.parentNode.removeChild(tr);
+        },
+        error: function(error) {
+            console.log('Error: ', error);
+        }
+
+    });
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
 
