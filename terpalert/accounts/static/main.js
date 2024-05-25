acff149603d4b82aaf57ef517927f8d46e342da5
Thomas Huitema <thuitema35@gmail.com>
2024-05-24 21:52:51 -0400

    Documentation

--- terpalert/accounts/static/main.js
+++ terpalert/accounts/static/main.js
@@ -1,10 +1,17 @@
 let alertTableBody;
 
+/**
+ * When the window is loaded, display the user's alerts in a table
+ */
 window.onload = function () {
     alertTableBody = document.getElementById('alert-table-body');
     getAlerts();
 }
 
+/**
+ * Send an Ajax request to retrieve the user's alerts,
+ * then display each one in a table row
+ */
 function getAlerts() {
     $.ajax({
         type: 'GET',
@@ -16,29 +23,35 @@ function getAlerts() {
                     <p>Nothing added yet!</p>
                 `
             }
-            data.forEach(element => {
-                console.log(element)
+
+            // Display each alert in a row in the table
+            data.forEach(alert => {
                 alertTableBody.innerHTML += `
-                    <tr data-alert-id="${element.id}">
-                        
-                        <td>${element.alert}</td>
+                    <tr data-alert-id="${alert.id}">
+                        <!-- Cell 1 contains the alert -->
+                        <td>${alert.alert}</td>
+                        <!-- Cell 2 contains the delete button -->
                         <td>
                             <button type="button" class="btn btn-danger" onclick="this.blur(); deleteAlert(this);">
                                 <i class="bi bi-trash"></i>
                             </button>
                         </td>
-                        
                     </tr>
                 `
             });
         },
         error: function (error) {
-            console.log(error);
+            alert('Something went wrong, please try again later');
         }
     })
 }
 
+/**
+ * Sends an Ajax request to delete the specified alert
+ * @param button Button clicked to delete alert
+ */
 function deleteAlert(button) {
+    // Show popup for user to confirm they want to delete alert
     if (confirm("Are you sure want to delete this alert?") == false) {
         return;
     }
@@ -51,33 +64,37 @@ function deleteAlert(button) {
         type: 'POST',
         url: '/accounts/delete-alert/',
         headers: {'X-CSRFToken': csrftoken},
-        data: {
+        data: { // sends the id of the alert
             'alert-id': tr.getAttribute('data-alert-id'),
         },
         success: function (response) {
-            console.log(response.data)
-            tr.parentNode.removeChild(tr);
+            tr.parentNode.removeChild(tr); // delete row from html
         },
         error: function (error) {
-            console.log('Error: ', error);
+            alert('Something went wrong, please try again later');
         }
-
     });
 }
 
+/**
+ * Adds row to top of table allowing the user to enter a new alert and save it
+ * @param button Button clicked to add alert
+ */
 function addAlert(button) {
-    button.disabled = true; // don't allow button to be clicked until the form is saved or cancelled
+    button.disabled = true; // don't allow the add button to be clicked until the new alert is saved or cancelled
 
     const table = document.getElementById('alert-table');
     const row = table.insertRow(1);
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
 
+    // cell1 contains the input field for the new alert
     cell1.innerHTML = `
         <input type="text" name="alert" id="alert-input" placeholder="Type here" required>
     `;
+    // cell2 contains the cancel and save buttons
     cell2.innerHTML = `
-        <button type="button" class="btn btn-light rounded-circle" onclick="cancelRow(this);">
+        <button type="button" class="btn btn-light rounded-circle" onclick="removeRow(this);">
             <i class="bi bi-x-circle"></i>
         </button>
         <button type="button" class="btn btn-success rounded-circle" id="save-btn" onclick="saveAlert(this);" disabled>
@@ -86,41 +103,47 @@ function addAlert(button) {
     `;
 
     // disable save button if the text box is empty
-    $('#alert-input').on('keyup', checkKeyworkInput);
+    $('#alert-input').on('keyup', checkKeywordInput);
 
+    // allow user to save alert by pressing enter key in addition to the save button
     const saveBtn = document.getElementById('save-btn');
-    $('#alert-input').keypress(function(event) {
-        if(event.keyCode == 13 || event.which == 13) {
-            if(saveBtn.disabled == false) {
+    $('#alert-input').keypress(function (event) {
+        if (event.keyCode == 13 || event.which == 13) { // 13 is the code for ENTER
+            if (saveBtn.disabled == false) {
                 saveAlert(saveBtn);
             }
         }
     })
 }
 
+/**
+ * Sends an Ajax request to save the alert entered by the user
+ * @param button Button clicked to save alert
+ */
 function saveAlert(button) {
-    const input = $('#alert-input').val();
+    const input = $('#alert-input').val(); // alert entered by user
     const csrftoken = getCookie('csrftoken');
 
     $.ajax({
         type: 'POST',
         url: '/accounts/save-alert/',
         headers: {'X-CSRFToken': csrftoken},
-        data: {
+        data: { // sends the alert name
             'alert': input
         },
         success: function (response) {
             if (response.success == true) {
-                // delete input row and insert new keyword row at top of table
-                cancelRow(button);
+                // delete input row and insert new alert row at top of table
+                removeRow(button);
                 const table = document.getElementById('alert-table');
                 const row = table.insertRow(1);
-                // row.id = response.id;
                 row.setAttribute('data-alert-id', response.id)
                 const cell1 = row.insertCell(0);
                 const cell2 = row.insertCell(1);
 
+                // cell1 contains the alert
                 cell1.innerHTML = response.alert;
+                // cell2 contains the delete button
                 cell2.innerHTML = `
                     <button type="button" class="btn btn-danger" onclick="this.blur(); deleteAlert(this);">
                         <i class="bi bi-trash"></i>
@@ -136,13 +159,20 @@ function saveAlert(button) {
     });
 }
 
-function cancelRow(button) {
+/**
+ * Deletes row containing button
+ * @param button Button in row to be removed
+ */
+function removeRow(button) {
     const tr = button.parentNode.parentNode;
     tr.parentNode.removeChild(tr);
     document.getElementById('add-button').disabled = false;
 }
 
-function checkKeyworkInput() {
+/**
+ * Disabled save button as long as the input field is empty
+ */
+function checkKeywordInput() {
     if ($('#alert-input').val().length > 0) {
         $('#save-btn').prop('disabled', false);
     } else {
@@ -150,6 +180,11 @@ function checkKeyworkInput() {
     }
 }
 
+/**
+ * Django-provided function to retrieve a cookie
+ * @param name Name of cookie to get
+ * @returns Value of cookie
+ */
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -165,6 +200,4 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
-
 
