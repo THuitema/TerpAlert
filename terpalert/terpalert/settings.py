2501fa08b77f2ee4033f21aa3731bd7ee82a751f
Thomas Huitema <thuitema35@gmail.com>
2024-05-28 01:01:21 -0400

    Fix issue where style.css wasn't being loaded in

--- terpalert/terpalert/settings.py
+++ terpalert/terpalert/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
