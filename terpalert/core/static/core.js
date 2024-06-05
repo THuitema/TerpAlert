2be3584d57bc8a733cd3c7e0498a3a1c04e77a49
Thomas Huitema <thuitema35@gmail.com>
2024-06-04 22:51:33 -0400

    Implement the search bar for today's menu on the landing page

--- terpalert/core/static/core.js
+++ terpalert/core/static/core.js
@@ -1,3 +1,71 @@
-$(document).ready(function() {
-   console.log('core!');
-});
+window.onload = function () {
+    // const searchInput = document.getElementById()
+    $('#food-input').autocomplete({
+        // Sends an Ajax request to gather menu items matching user's input
+        source: getMenu,
+        // Bold characters in results that match search term (case-insensitive)
+        open: function (event, ui) {
+            const data = $(this).data('ui-autocomplete');
+            data.menu.element.find('li').each(function () {
+                const me = $(this);
+                const keywords = data.term.split(' ').join('|');
+                let textWrapper = me.find('.ui-menu-item-wrapper');
+                let text = textWrapper.text();
+                let newTextHtml = text.replace(new RegExp("(" + keywords + ")", "gi"), '<b>$1</b>');
+                textWrapper.html(newTextHtml);
+            });
+        },
+        // Save item when selected
+        select: function (event, ui) {
+            const input = $('#food-input');
+            input.val(ui.item.label);
+            checkAlertExists(input.val());
+        },
+        delay: 200,
+        minLength: 1,
+    })
+}
+
+/**
+ * Sends an Ajax request to get relevant menu items based on the search term
+ * @param request Contains the search term from the user
+ * @param response Callback function to send labels and values to the autocomplete menu
+ */
+function getMenu(request, response) {
+    return $.ajax({
+        type: 'GET',
+        url: '/accounts/load-menu/',
+        data: {
+            'term': request.term,
+        },
+        success: function (data) {
+            let results = $.map(data.data, function (value, key) {
+                return {
+                    label: value.label, // label and value is the name of the menu item
+                    value: value.label
+                }
+            });
+            response(results.slice(0, 10)); // limit to 10 results
+        }
+    })
+}
+
+function checkAlertExists(input) {
+    $.ajax({
+        type: 'GET',
+        url: '/check-for-alert',
+        data: {
+            'item': input,
+        },
+        success: function (data) {
+            $('#food-input').val('');
+            const result = document.getElementById('food-input-results') // $('#food-input-results');
+
+            if (data.found == true) {
+                result.innerHTML = '🚨 ' + data.item + ' is being served at ' + data.dining_halls + ' 🚨';
+            } else {
+                result.innerHTML = "Sorry, " + data.item + " isn't being served today";
+            }
+        }
+    })
+}
--- terpalert/core/views.py
+++ terpalert/core/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-import requests
+from django.shortcuts import render, redirect
+from accounts.models import DailyMenu, Menu
+from django.http import HttpResponse, JsonResponse
+from datetime import date
 
 
-# Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def check_for_alert(request):
+    if 'item' in request.GET:
+        item = request.GET['item']
+
+        menu_item = Menu.objects.get(item=item)  # Get Menu object for that item
+        daily_menu_item = DailyMenu.objects.filter(menu_item_id=menu_item.id, date=date.today())
+
+        if daily_menu_item.exists():
+            data = {'found': True, 'item': item}
+
+            # Add dining halls the alert is applicable to
+            dining_halls = []
+            if daily_menu_item[0].yahentamitsi_dining_hall:
+                dining_halls.append('Yahentamitsi')
+            if daily_menu_item[0].south_dining_hall:
+                dining_halls.append('South')
+            if daily_menu_item[0].two_fifty_one_dining_hall:
+                dining_halls.append('251')
+
+            data['dining_halls'] = ', '.join(dining_halls)
+        else:
+            data = {'found': False, 'item': item}
+
+        return JsonResponse(data)
+    else:
+        return redirect('home')
