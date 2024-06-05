86e7aaa47cd8294bce3f434a0e926f1be36ab328
Thomas Huitema <thuitema35@gmail.com>
2024-06-04 22:56:08 -0400

    Documentation for whole file

--- terpalert/core/views.py
+++ terpalert/core/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
 from accounts.models import DailyMenu, Menu
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from datetime import date
 
 
 def home(request):
+    """
+    Renders the website landing page
+    """
     return render(request, 'index.html')
 
 
 def check_for_alert(request):
+    """
+    Handle the Ajax request to check if an item is in DailyMenu table for today's date
+
+    :return: JsonResponse with fields "found", "item", and "dining_halls" (if found is true)
+    """
     if 'item' in request.GET:
         item = request.GET['item']
 
@@ -16,9 +24,10 @@ def check_for_alert(request):
         daily_menu_item = DailyMenu.objects.filter(menu_item_id=menu_item.id, date=date.today())
 
         if daily_menu_item.exists():
+            # Item is being served today
             data = {'found': True, 'item': item}
 
-            # Add dining halls the alert is applicable to
+            # Add dining halls the item is applicable to
             dining_halls = []
             if daily_menu_item[0].yahentamitsi_dining_hall:
                 dining_halls.append('Yahentamitsi')
@@ -29,6 +38,7 @@ def check_for_alert(request):
 
             data['dining_halls'] = ', '.join(dining_halls)
         else:
+            # Item isn't being served today
             data = {'found': False, 'item': item}
 
         return JsonResponse(data)
