b09b1e6292edd971b22ec3af306b1cffe21323e1
Thomas Huitema <thuitema35@gmail.com>
2024-06-10 21:38:03 -0400

    Add custom password reset templates to urls.py

--- terpalert/accounts/urls.py
+++ terpalert/accounts/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('account/', views.account, name='account'),
     path('signup/', views.create_profile, name='signup'),
@@ -11,13 +12,18 @@ urlpatterns = [
     path('delete-alert/', views.delete_alert, name='delete-alert'),
     path('save-alert/', views.save_alert, name='save-alert'),
     path('load-menu/', views.load_menu, name='load-menu'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+         template_name='registration/password_reset.html'),
          name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+         template_name='registration/password_reset_sent.html'),
          name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+         template_name='registration/password_reset_form.html'),
          name='password_reset_confirm'),
-    path('reset/done', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+         template_name='registration/password_reset_done.html'),
          name='password_reset_complete'),
 
 ]
