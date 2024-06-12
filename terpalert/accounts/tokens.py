6d837a2f22b25f356ed0746073eae540ad3980e3
Thomas Huitema <thuitema35@gmail.com>
2024-06-12 15:18:36 -0400

    Documentation and cleanup of email verification

--- terpalert/accounts/tokens.py
+++ terpalert/accounts/tokens.py
@@ -3,10 +3,13 @@ from six import text_type
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
+    """
+    Override Django's PasswordResetTokenGenerator to generate tokens for email verification
+    """
     def _make_hash_value(self, user, timestamp):
         return (
             text_type(user.pk) + text_type(timestamp) + text_type(user.email_is_verified)
         )
 
 
-account_activation_token = TokenGenerator()
+account_activation_token = TokenGenerator()  # Generate new token for the current user
