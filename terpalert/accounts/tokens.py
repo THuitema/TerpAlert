ad034c8e336425c1bc7d4645e6dd0166a2b5d78a
Thomas Huitema <thuitema35@gmail.com>
2024-06-12 16:25:20 -0400

    Fix changed model field name that we use to check if a user is active

--- terpalert/accounts/tokens.py
+++ terpalert/accounts/tokens.py
@@ -8,7 +8,7 @@ class TokenGenerator(PasswordResetTokenGenerator):
     """
     def _make_hash_value(self, user, timestamp):
         return (
-            text_type(user.pk) + text_type(timestamp) + text_type(user.email_is_verified)
+            text_type(user.pk) + text_type(timestamp) + text_type(user.is_active)
         )
 
 
