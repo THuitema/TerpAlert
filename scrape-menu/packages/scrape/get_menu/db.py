2b51a5954496c0c6bb16e31f8ad236bf1b02bad3
Thomas Huitema <thuitema35@gmail.com>
2024-06-25 22:15:05 -0400

    Add Digital Ocean environment variables

--- scrape-menu/packages/scrape/get_menu/db.py
+++ scrape-menu/packages/scrape/get_menu/db.py
@@ -5,12 +5,13 @@ import psycopg
 def connect():
     try:
         config = 'dbname={0} user={1} password={2} host={3} port={4}'.format(
-            os.environ.get('database'),
-            os.environ.get('username'),
-            os.environ.get('password'),
-            os.environ.get('host'),
-            os.environ.get('port')
+            os.environ.get('DB_NAME'),
+            os.environ.get('DB_USER'),
+            os.environ.get('DB_PASSWORD'),
+            os.environ.get('DB_HOST'),
+            os.environ.get('DB_PORT')
         )
+        return {'config': config}
         with psycopg.connect(config) as conn:
             return conn
 
