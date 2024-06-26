55bcebc07b12f70262a0421c99ba84f78f178e8b
Thomas Huitema <thuitema35@gmail.com>
2024-06-25 22:07:08 -0400

    Add connect()

--- scrape-menu/packages/scrape/get_menu/__main__.py
+++ scrape-menu/packages/scrape/get_menu/__main__.py
@@ -1,9 +1,8 @@
 from datetime import date
 import requests
 from bs4 import BeautifulSoup
-import psycopg
 import os
-from db import setup
+from db import connect
 from dining_hall import DiningHall, Menu
 
 # Constants for dining hall info
@@ -14,13 +13,14 @@ HALLS = {SOUTH: 16, YAHENTAMITSI: 19, TWO_FIFTY_ONE: 51}
 
 
 def main(args):
-    the_y = DiningHall(name=YAHENTAMITSI, location_num=HALLS[YAHENTAMITSI])
-    south = DiningHall(name=SOUTH, location_num=HALLS[SOUTH])
-    two_fifty_one = DiningHall(name=TWO_FIFTY_ONE, location_num=HALLS[TWO_FIFTY_ONE])
-
-    menu = Menu([the_y, south, two_fifty_one])
-    menu.create_menu()
-    return {'Total Menu': str(menu)}
+    return {'success': True}
+    # the_y = DiningHall(name=YAHENTAMITSI, location_num=HALLS[YAHENTAMITSI])
+    # south = DiningHall(name=SOUTH, location_num=HALLS[SOUTH])
+    # two_fifty_one = DiningHall(name=TWO_FIFTY_ONE, location_num=HALLS[TWO_FIFTY_ONE])
+    #
+    # menu = Menu([the_y, south, two_fifty_one])
+    # menu.create_menu()
+    # return {'Total Menu': str(menu)}
 
 
 
--- scrape-menu/packages/scrape/get_menu/db.py
+++ scrape-menu/packages/scrape/get_menu/db.py
@@ -1,4 +1,20 @@
+import os
 import psycopg
 
-def setup():
-    return {'psycopg': str(psycopg)}
+
+def connect():
+    try:
+        config = 'dbname={0} user={1} password={2} host={3} port={4}'.format(
+            os.environ.get('database'),
+            os.environ.get('username'),
+            os.environ.get('password'),
+            os.environ.get('host'),
+            os.environ.get('port')
+        )
+        with psycopg.connect(config) as conn:
+            return conn
+
+    except (psycopg.DatabaseError, Exception) as e:
+        print(e)
+
+
