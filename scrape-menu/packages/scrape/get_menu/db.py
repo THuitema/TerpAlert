260d077c3e48d964b7cd4511c937d62bcbb77ce9
Thomas Huitema <thuitema35@gmail.com>
2024-06-26 23:03:36 -0400

    Configuring Heroku database

--- scrape-menu/packages/scrape/get_menu/db.py
+++ scrape-menu/packages/scrape/get_menu/db.py
@@ -1,21 +1,23 @@
 import os
-import psycopg
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
 def connect():
     try:
-        config = 'dbname={0} user={1} password={2} host={3} port={4} sslmode=require'.format(
-            os.environ.get('DB_NAME'),
-            os.environ.get('DB_USER'),
-            os.environ.get('DB_PASSWORD'),
-            os.environ.get('DB_HOST'),
-            os.environ.get('DB_PORT')
-        )
+        # config = 'dbname={0} user={1} password={2} host={3} port={4} sslmode=require'.format(
+        #     os.environ.get('DB_NAME'),
+        #     os.environ.get('DB_USER'),
+        #     os.environ.get('DB_PASSWORD'),
+        #     os.environ.get('DB_HOST'),
+        #     os.environ.get('DB_PORT')
+        # )
 
-        with psycopg.connect(config) as conn: # os.environ.get('DB_URL')
+        with psycopg2.connect(DATABASE_URL, sslmode='require') as conn: # os.environ.get('DB_URL')
             return conn
 
-    except (psycopg.DatabaseError, Exception) as e:
+    except (psycopg2.DatabaseError, Exception) as e:
         print(e)
 
 
