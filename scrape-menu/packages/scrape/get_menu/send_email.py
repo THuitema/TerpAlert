b4f594c34b5f4714103a4c146577c26003dc0c73
Thomas Huitema <thuitema35@gmail.com>
2024-06-29 16:18:39 -0400

    Add send_alert() and incorporate Mailgun environment variables

--- scrape-menu/packages/scrape/get_menu/send_email.py
+++ scrape-menu/packages/scrape/get_menu/send_email.py
@@ -5,10 +5,22 @@ MAILGUN_API = os.environ['MAILGUN_API']
 
 
 def send_simple_message():
+    from_email = "Excited User <" + os.environ['MAILGUN_EMAIL'] + ">"
     return requests.post(
-        "https://api.mailgun.net/v3/terpalert.xyz/messages",
+        os.environ['MAILGUN_URL'],
         auth=("api", MAILGUN_API),
-        data={"from": "Excited User <mailgun@terpalert.xyz>",
+        data={"from": from_email,
               "to": ["thuitema35@gmail.com"],
               "subject": "Hello",
               "text": "Testing some Mailgun awesomeness! From, TerpAlert"})
+
+
+def send_alert(to_email: str, alerts: list[str]):
+    from_email = "TerpAlert <" + os.environ['MAILGUN_EMAIL'] + ">"
+    return requests.post(
+        os.environ['MAILGUN_URL'],
+        auth=("api", MAILGUN_API),
+        data={"from": from_email,
+              "to": [to_email],
+              "subject": "You have dining hall alerts!",
+              "text": alerts})
