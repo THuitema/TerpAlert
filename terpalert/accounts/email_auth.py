d824dd1eda7c4b597c3459d27ae93f61b621bb2f
Thomas Huitema <thuitema35@gmail.com>
2024-07-05 14:46:39 -0400

    Integrate Mailgun API for email verification

---
+++ terpalert/accounts/email_auth.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import json
+
+MAILGUN_API = os.environ['MAILGUN_API']
+
+
+def send_verification_email(to_email: str, token: str):
+    from_email = "TerpAlert <" + os.environ['MAILGUN_EMAIL'] + ">"
+    template = "email verification"
+    verification_url = 'https://terpalert.xyz/accounts/auth/' + token
+
+    return requests.post(
+        os.environ['MAILGUN_URL'],
+        auth=("api", MAILGUN_API),
+        data={"from": from_email,
+              "to": [to_email],
+              "subject": "Verify email",
+              "template": template,
+              "t:variables": '{"verification_url": ' + verification_url + '}'
+              }
+    )
