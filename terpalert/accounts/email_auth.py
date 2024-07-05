e6a7109d00d1edb557d0b029ec917b8ba670640e
Thomas Huitema <thuitema35@gmail.com>
2024-07-05 15:49:34 -0400

    Debugging

--- terpalert/accounts/email_auth.py
+++ terpalert/accounts/email_auth.py
@@ -6,19 +6,22 @@ MAILGUN_API = os.environ['MAILGUN_API']
 
 
 def send_verification_email(to_email: str, token: str):
-    from_email = os.environ['MAILGUN_EMAIL']
-    template = "email verification"
-    verification_url = 'https://terpalert.xyz/accounts/verify-email-confirm/' + token
+    try:
+        from_email = os.environ['MAILGUN_EMAIL']
+        template = "email verification"
+        verification_url = 'https://terpalert.xyz/accounts/verify-email-confirm/' + token
 
-    return requests.post(
-        url=os.environ['MAILGUN_URL'],
-        auth=("api", MAILGUN_API),
-        data={"from": from_email,
-              "to": [to_email],
-              "subject": "Verify your account",
-              "template": template,
-              # "t:variables": '{"alerts": ' + json.dumps(verification_url) + '}'
-              })
+        return requests.post(
+            url=os.environ['MAILGUN_URL'],
+            auth=("api", MAILGUN_API),
+            data={"from": from_email,
+                  "to": [to_email],
+                  "subject": "Verify your account",
+                  "template": template,
+                  # "t:variables": '{"alerts": ' + json.dumps(verification_url) + '}'
+                  })
+    except Exception as e:
+        return e
 
     # return requests.post(
     #     os.environ['MAILGUN_URL'],
