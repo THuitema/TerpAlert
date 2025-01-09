0a5e100d9c08fd699688f8cca00f1888ee32bc7e
Thomas Huitema <thuitema35@gmail.com>
2025-01-09 01:37:45 -0500

    Add "serving_size" field

--- terpalert/accounts/models.py
+++ terpalert/accounts/models.py
@@ -82,6 +82,7 @@ class UniqueMenuItem(models.Model):
     protein = models.FloatField(null=True, blank=True)
     carbs = models.FloatField(null=True, blank=True)
     fats = models.FloatField(null=True, blank=True)
+    serving_size = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
