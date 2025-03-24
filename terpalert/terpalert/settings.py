5142b7875a9dd47cce88f5317662797a5357589c
Thomas Huitema <thuitema35@gmail.com>
2025-03-24 00:10:54 -0400

    CORS

--- terpalert/terpalert/settings.py
+++ terpalert/terpalert/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware'
 ]
 
