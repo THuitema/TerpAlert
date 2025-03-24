046b24e24297464716ccaf2eb6f0abfd9db3a062
Thomas Huitema <thuitema35@gmail.com>
2025-03-24 00:06:49 -0400

    Change format of allergens query parameter to comma-separated

--- terpalert/terpalert/settings.py
+++ terpalert/terpalert/settings.py
@@ -76,7 +76,33 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    '*' #'http://localhost:3000/',  # Example origin
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+    'OPTIONS',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'terpalert.urls'
 
